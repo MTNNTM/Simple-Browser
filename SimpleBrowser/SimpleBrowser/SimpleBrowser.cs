08598fe97ab9725fa4c2ea1b78217a6775ad4bf4
Matan <aa0bbcc@gmail.com>
2018-12-29 20:23:42 +0200

    Create a Simple Browser.

---
+++ SimpleBrowser/SimpleBrowser/SimpleBrowser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleBrowser
+{
+    public partial class SimpleBrowser : Form
+    {
+        public SimpleBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackward_Click(object sender, EventArgs e)
+        {
+            wb.GoBack();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            wb.Navigate("www.google.com");
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            wb.GoForward();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            wb.Refresh();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string address = tbSiteAddress.ToString();
+            wb.Navigate(address);
+        }
+    }
+}
